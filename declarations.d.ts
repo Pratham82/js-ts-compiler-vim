e5aeee61688e1a38a51677095eb930dfeb97dcce
Prathamesh Mali <prathamesh@rideblitz.com>
2025-06-18 13:47:32 +0530

    feat: 🎸 added project initialisation

---
+++ declarations.d.ts
@@ -0,0 +1,12 @@
+declare module "monaco-vim" {
+  import * as monaco from "monaco-editor"
+
+  export interface VimMode {
+    dispose: () => void
+  }
+
+  export default function initVimMode(
+    editor: monaco.editor.IStandaloneCodeEditor,
+    statusBar: HTMLElement
+  ): VimMode
+}
